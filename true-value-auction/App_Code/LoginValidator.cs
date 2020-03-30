18202d3bd00d3a449bca629084a4a83820dacb0d
TJKozlowski <tkozl804@uwsp.edu>
2020-03-30 17:59:20 -0500

    Implementing code

--- true-value-auction/App_Code/LoginValidator.cs
+++ true-value-auction/App_Code/LoginValidator.cs
@@ -1,1 +1,26 @@
-﻿
+﻿namespace truevalueauction.App_Code
+{
+    public class LoginValidator
+    {
+
+        public static bool userNameIsValid(User user)
+        {
+            return false;
+        }
+
+        public static bool passwordIsValid(User user)
+        {
+            return false;
+        }
+
+        public static string handleExceptions(User user)
+        {
+
+
+
+
+            return "test";
+        }
+    }
+}
+
--- true-value-auction/App_Code/User.cs
+++ true-value-auction/App_Code/User.cs
@@ -1,1 +1,25 @@
 ﻿
+namespace truevalueauction.App_Code
+{
+    public class User
+    {
+        private string userName;
+        private string password;
+
+        public User(string userName, string password)
+        {
+            this.userName = userName;
+            this.password = password;
+        }
+
+        public string GetUserName()
+        {
+            return userName;
+        }
+
+        public string GetPassword()
+        {
+            return password;
+        }
+    }
+}
